9cdcbad8d818aabd70eab1a896b9e24ffd8bb66e
csetowhid <towhidhasang1@gmail.com>
2022-06-19 10:57:52 +0600

    Logo Update

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -106,7 +106,7 @@
                                 <div class="widget-content-left">
                                     <div class="btn-group">
                                         <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
-                                            <img width="42" class="rounded-circle" src="{{asset('backend/images/avatars/1.jpg')}}" alt="">
+                                            <img width="42" class="rounded-circle" src="{{asset('backend/images/avatars/towhid.png')}}" alt="">
                                             <i class="fa fa-angle-down ml-2 opacity-8"></i>
                                         </a>
                                         <div tabindex="-1" role="menu" aria-hidden="true" class="rm-pointers dropdown-menu-lg dropdown-menu dropdown-menu-right">
@@ -120,7 +120,7 @@
                                                                     <img width="42" class="rounded-circle" src="assets/images/avatars/1.jpg" alt="">
                                                                 </div>
                                                                 <div class="widget-content-left">
-                                                                    <div class="widget-heading">Alina Mcloughlin</div>
+                                                                    <div class="widget-heading">Towhid Hasan</div>
                                                                     <div class="widget-subheading opacity-8">A short profile description</div>
                                                                 </div>
                                                                 {{-- <div class="widget-content-right mr-2">
@@ -169,13 +169,8 @@
                                     </div>
                                 </div>
                                 <div class="widget-content-left  ml-3 header-user-info">
-                                    <div class="widget-heading"> Alina Mclourd </div>
-                                    <div class="widget-subheading"> VP People Manager </div>
-                                </div>
-                                <div class="widget-content-right header-user-info ml-3">
-                                    <button type="button" class="btn-shadow p-1 btn btn-primary btn-sm show-toastr-example">
-                                        <i class="fa text-white fa-calendar pr-1 pl-1"></i>
-                                    </button>
+                                    <div class="widget-heading"> Towhid Hasan </div>
+                                    <div class="widget-subheading"> Backend Engineer </div>
                                 </div>
                             </div>
                         </div>
